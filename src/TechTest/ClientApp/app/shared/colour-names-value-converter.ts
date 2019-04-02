fb0558943b5cd0e44b95d153b5081ae3238b82c7
lionel-rowe <lionel.rowe@gmail.com>
2019-04-03 02:58:10 +0800

    Implement toView method

--- src/TechTest/ClientApp/app/shared/colour-names-value-converter.ts
+++ src/TechTest/ClientApp/app/shared/colour-names-value-converter.ts
@@ -2,18 +2,11 @@ import { IColour } from '../people/interfaces/icolour';
 
 export class ColourNamesValueConverter {
 
-  toView(colours: IColour[]) {
+  toView(colours: IColour[]): string {
 
-    // TODO: Step 4
-    //
-    // Implement the value converter function.
-    // Using the colours parameter, convert the list into a comma
-    // separated string of colour names. The names should be sorted
-    // alphabetically and there should not be a trailing comma.
-    //
-    // Example: 'Blue, Green, Red'
+    // DONE: Step 4
 
-    return 'Todo';
+    return colours.map(c => c.name).sort().join(', ');
   }
 
 }
